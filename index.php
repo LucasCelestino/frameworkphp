c6985a8821b78783fe1a13236247930a918beef6
Lucas Celestino <lucasrashido@gmail.com>
2022-03-25 17:23:28 -0300

    modificando recurso de rotas

--- index.php
+++ index.php
@@ -4,13 +4,25 @@ session_start();
 require("vendor/autoload.php");
 require("app/configuration.php");
 
-use Slim\Slim;
+use MiladRahimi\PhpRouter\Router;
+use MiladRahimi\PhpRouter\Exceptions\RouteNotFoundException;
+use Laminas\Diactoros\Response\HtmlResponse;
 
-$router = new Slim();
+use App\Controllers\HomeController;
 
-$router->get('/', function (){
-    $homeController = new \App\Controllers\HomeController();
-    $homeController->index();
-});
+$router = Router::create();
 
-$router->run();
+
+$router->get('/user', [HomeController::class, 'index']);
+
+$router->post('/cadastro', [HomeController::class, 'cadastroPost']);
+
+try {
+    $router->dispatch();
+} catch (RouteNotFoundException $e) {
+    // It's 404!
+    $router->getPublisher()->publish(new HtmlResponse('Not found.', 404));
+} catch (Throwable $e) {
+    // Log and report...
+    $router->getPublisher()->publish(new HtmlResponse('Internal error.', 500));
+}
