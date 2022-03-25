4ece520efb33996b171b36ea182e1ee706883208
Lucas Celestino <lucasrashido@gmail.com>
2022-03-25 17:27:17 -0300

    adicionando estrutura inicial para um controller

---
+++ app/src/Controllers/UserController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controllers;
+
+class UserController extends Controller
+{
+    /* 
+    GET
+    Show a list of the resource
+    */ 
+    public function index()
+    {
+        $data['name'] = 'Fulano';
+        $this->render('cadastro.html', $data);
+    }
+
+    /*
+     GET
+     Show a form to create new resource
+    */ 
+    public function create()
+    {
+        
+    }
+
+    /*
+    POST
+    Store a newly created resource in database
+    */ 
+    public function store()
+    {
+        
+    }
+
+    /*
+     GET
+     Show one resource
+    */  
+    public function show(int $id)
+    {
+        
+    }
+
+    /*
+    GET
+    Show a form to edit one resource
+    */ 
+    public function edit(int $id)
+    {
+        
+    }
+
+    /*
+    PUT/PATCH
+    Update the specified resource in database
+    */ 
+    public function update()
+    {
+        
+    }
+    /*
+    DELETE
+    Delete the specified resource from database
+    */ 
+    public function destroy()
+    {
+        
+    }
+}
